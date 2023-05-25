3f071ab945c2fadfab8e63c843811d165542bff4
Artem Sky <ok123x@yandex.ru>
2023-05-25 14:32:18 +0300

    add eurodollars.js

--- eurodollars.js
+++ eurodollars.js
@@ -1,1 +1,9 @@
 const eurosCount = 100;
+
+let eurostoDollars = 1.25;
+let dollarstoEuros = 60;
+let evrDol = eurosCount * eurostoDollars;
+let dolRub = evrDol * dollarstoEuros;
+
+console.log(evrDol);
+console.log(dolRub);
