c8d9c884755e8d03371c685c097835943ec5fec5
Artem Sky <ok123x@yandex.ru>
2023-06-07 18:15:04 +0300

    add switch.js

---
+++ switch.js
@@ -0,0 +1,14 @@
+const getNumberExplanation = (number) => {
+	switch (number) {
+	case 777:
+	return 'lacky number';
+	case 8:
+	return 'infinity';
+	case 13:
+	return 'justNumber';
+	case 23:
+	return 'Mikle Jordan';
+	default:
+	return 'orher number';
+}
+};
