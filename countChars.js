1f5113f28dcfc4b1ca3c7d99069b1393efd8adc8
Artem Sky <ok123x@yandex.ru>
2023-06-09 19:05:37 +0300

    add countChars.js

---
+++ countChars.js
@@ -0,0 +1,13 @@
+const countChars = (str, char) => {
+    let i = 0;
+    let count = 0;
+    while (i < str.length) {
+        if (str[i].toLowerCase() === char.toLowerCase()){
+            count += 1;
+        }
+
+        i += 1;
+    }
+
+    return count;
+};
